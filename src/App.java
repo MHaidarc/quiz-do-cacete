c654d40dc6f5c5006ff020d7863c8b03789a1e68
MATEUS HAIDAR CARVALHO <3036308@itp.local>
2024-12-18 11:44:46 -0300

    tratei todas excecoes... so falta perguntas

--- src/App.java
+++ src/App.java
@@ -52,137 +52,218 @@ public class App {
             } while (inputNome.isEmpty());
 
             //perguntinhas
-            System.out.println("1. Qual a velocidade máxima permitida em vias urbanas, salvo sinalização em contrário?");
-            System.out.println("A) 30 km/h");
-            System.out.println("B) 50 km/h"); //certo
-            System.out.println("C) 60 km/h");
-            System.out.println("D) 80 km/h");
-            System.out.println("E) 100 km/h");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
-            
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
 
-            System.out.println("2. O uso do cinto de segurança é obrigatório para: ");
-            System.out.println("A) Apenas o motorista");
-            System.out.println("B) Apenas os passageiros da frente");
-            System.out.println("C) Apenas para crianças");
-            System.out.println("D) Todos os ocupantes do veículo"); //certo
-            System.out.println("E) Somente para passageiros acima de 18 anos");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("3. Qual a idade mínima para conduzir uma motocicleta no Brasil?");
-            System.out.println("A) 14 Anos");
-            System.out.println("B) 16 Anos");
-            System.out.println("C) 17 Anos");
-            System.out.println("D) 18 Anos"); //certo
-            System.out.println("E) 21 Anos");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("4. Qual o limte de pontos na CNH para que o condutor tenha sua habilitação suspensa, considerando o limite padrão?");
-            System.out.println("A) 20 pontos");
-            System.out.println("B) 30 pontos");
-            System.out.println("C) 40 pontos"); //certo
-            System.out.println("D) 50 pontos");
-            System.out.println("E) 60 pontos");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("5. Segundo o Código de Trânsito Brasileiro (CTB), o que caracteriza uma infração gravíssima multiplicada por três (fator multiplicador)?");
-            System.out.println("A) Estacionar em local proibido");
-            System.out.println("B) Ultrapassar em faixa contínua");
-            System.out.println("C) irigir sob efeito de álcool"); //certo
-            System.out.println("D) Não usar cinto de segurança");
-            System.out.println("E) Transitar na contramão em via de mão única");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("6. Qual dessas infrações pode levar à cassação direta da CNH?");
-            System.out.println("A) Não portar documento do veículo");
-            System.out.println("B) Estacionar em vaga de idoso sem permissão");
-            System.out.println(""); //certo
-            System.out.println("");
-            System.out.println("");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println(""); //certo
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println(""); //certo
-            System.out.println("");
-            System.out.println("");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println(""); 
-            System.out.println(""); //certo
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println(""); //certo
-            System.out.println("");
-            System.out.print("Resposta: ");
-            input= ler.next().charAt(0);
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
 
-            if (input == 'b' || input == 'B') {
-                pontos++;
-            }
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
 
-            //printa os pontos, adiciona-os em uma lista e zera-os
+            do {
+                try {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println(""); //certo
+                    System.out.print("Resposta: ");
+                    input= ler.next().charAt(0);
+
+                    if (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E') {
+                        throw new RuntimeException();
+                    } else if (input == 'b' || input == 'B') {
+                        pontos++;
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Resposta invalida. Tente novamente: ");
+                }
+            } while (input != 'a' && input != 'A' && input != 'b' && input != 'B' && input != 'c' && input != 'C' && input != 'd' && input != 'D' && input != 'e' && input != 'E');
+
+            //printa os pontos, adiciona-os em uma lista e zera-os para o proximo jogador
             System.out.println("você fez " + pontos + " pontos");
             lista_de_pontos.add(pontos);
             pontos = 0;
@@ -211,9 +292,11 @@ public class App {
 
 
         //printar ranking
-        System.out.println("Rodada terminada!\nRanking:");
-        for (int i = 0; i < nomes.size(); i++) {
-            System.out.printf("%sº lugar: %s - %s pontos\n", i+1, nomes.get(i), lista_de_pontos.get(i));
+        if (nomes.isEmpty() == false) {
+            System.out.println("Rodada terminada!\nRanking:");
+            for (int i = 0; i < nomes.size(); i++) {
+                System.out.printf("%sº lugar: %s - %s pontos\n", i+1, nomes.get(i), lista_de_pontos.get(i));
+            }
         }
         
         ler.close();
